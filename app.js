4a54355267886aaf437982b6fa425e48d69ecfa4
Anissa Nishioka <ani.nishioka@gmail.com>
2024-02-13 16:35:34 -0800

    allow for adding and removing gifs

--- app.js
+++ app.js
@@ -9,21 +9,41 @@ console.log("Let's get this party started!");
  * put response on page
  */
 
-const submitButton = $("#submit-button")
-submitButton.on("click", getGif);
+const submitButton = $("#submit-button");
+const clearButton = $("#clear-button");
+submitButton.on("click", getAndDisplayGif);
+clearButton.on("click", clearGifs);
 
-async function getGif(evt) {
+/** Gets search input, fetches gif, and displays in DOM. */
+async function getAndDisplayGif(evt) {
   evt.preventDefault();
+  const gif = await getGif();
+  displayGif(gif);
+}
 
+/** Grabs search input from form and fetch gif data from giphy. */
+async function getGif() {
   const searchInput = $("#search-input").val();
   const params = new URLSearchParams({
     q: searchInput,
     limit: "1",
-    api_key: "BdtCGyMWhBe2CpRs68q8DzWVZabQi9j5"});
-  console.log(params);
+    api_key: "BdtCGyMWhBe2CpRs68q8DzWVZabQi9j5"
+  });
 
   const response = await fetch(`http://api.giphy.com/v1/gifs/search?${params}`);
-  const gif = await response.json();
-  console.log(gif);
-  //displayGif(gif.images.original.url);
+  const gifData = await response.json();
+  const gif = gifData.data[0].images.original.url;
+  return gif;
+}
+
+/** Takes url for gif and display gif in DOM. */
+function displayGif(gif) {
+  const image = $("<img>");
+  image.attr("src", gif);
+  $("#gif-container").append(image);
+}
+
+/** Clears all gifs from DOM. */
+function clearGifs() {
+  $("#gif-container").clear();
 }
