35f2a8b709e37f7d4724d4f2b01a1eca113be846
David Sapiro <dsap48@gmail.com>
2024-02-13 16:17:51 -0800

    added built form and fetched gif

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 console.log("Let's get this party started!");
+
+/**
+ * event listener on submit button
+ * grab search input
+ * create parameters object
+ * use parameters to fetch
+ * handle response
+ * put response on page
+ */
+
+const submitButton = $("#submit-button")
+submitButton.on("click", getGif);
+
+async function getGif(evt) {
+  evt.preventDefault();
+
+  const searchInput = $("#search-input").val();
+  const params = new URLSearchParams({
+    q: searchInput,
+    limit: "1",
+    api_key: "BdtCGyMWhBe2CpRs68q8DzWVZabQi9j5"});
+  console.log(params);
+
+  const response = await fetch(`http://api.giphy.com/v1/gifs/search?${params}`);
+  const gif = await response.json();
+  console.log(gif);
+  //displayGif(gif.images.original.url);
+}
